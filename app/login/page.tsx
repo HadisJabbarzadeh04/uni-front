c7b1af56f6001d5e8b8f49f443f2c64d65b8364e
HadisJabbarzadeh04 <hadis.ai.eng2004@gmail.com>
2026-06-13 18:36:09 +0330

    Add new changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,41 +1,55 @@
 "use client";
 
 import { useState } from "react";
-import { login } from "@/services/auth";
+import { login } from "@/services/authService";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-  const [username, setUsername] = useState("");
+  const [nationalCode, setNationalCode] = useState("");
   const [password, setPassword] = useState("");
+
   const router = useRouter();
 
   async function handleLogin() {
-  try {
-    const res = await login();
+    try {
+      if (nationalCode.length !== 10) {
+        alert("National code must be 10 digits");
+        return;
+      }
 
-    localStorage.setItem("accessToken", res.accessToken);
-    localStorage.setItem("refreshToken", res.refreshToken);
+      const res = await login(
+        Number(nationalCode),
+        password
+      );
 
-    router.push("/dashboard");
-  } catch (err) {
-    console.log("LOGIN FAILED");
+      localStorage.setItem("accessToken", res.accessToken);
+      localStorage.setItem("refreshToken", res.refreshToken);
+
+      router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+      console.log("LOGIN FAILED");
+    }
   }
-}
 
   return (
     <div>
       <input
-        placeholder="username"
-        onChange={(e) => setUsername(e.target.value)}
+        placeholder="National Code"
+        value={nationalCode}
+        onChange={(e) => setNationalCode(e.target.value)}
       />
 
       <input
-        placeholder="password"
+        placeholder="Password"
         type="password"
+        value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
 
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
---
+++ services/authService.ts
@@ -0,0 +1,28 @@
+import { LoginDTO } from "@/types/auth/login";
+import { ApiResponse } from "@/types/api";
+
+const BASE_URL = "http://localhost:5000/api/auth/web";
+
+export async function login(
+  nationalCode: number,
+  password: string
+): Promise<LoginDTO> {
+  const res = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nationalCode,
+      password,
+    }),
+  });
+
+  const result: ApiResponse<LoginDTO> = await res.json();
+
+  if (!result.ok || !result.data) {
+    throw new Error(result.message || "Login failed");
+  }
+
+  return result.data;
+}
