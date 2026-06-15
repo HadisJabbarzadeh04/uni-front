0bdc63914e9246bd6539808c0ee2a778120ab650
HadisJabbarzadeh04 <hadis.ai.eng2004@gmail.com>
2026-06-15 23:43:40 +0330

    Add isAuth function to auth in lib

--- services/authService.ts
+++ services/authService.ts
@@ -1,32 +1,3 @@
-// export async function login(nationalCode: number, password: string) {
-  
-//   console.log("FAKE LOGIN:", nationalCode, password);
-
-//   return {
-//     accessToken: "fake-access-token",
-//     refreshToken: "fake-refresh-token",
-//   };
-// }
-
-// export async function getUserInfo(): Promise<any> {
-//   const token = localStorage.getItem("accessToken");
-
-//   const res = await fetch("http://localhost:5000/api/auth/web/getUserInfo", {
-//     method: "GET",
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
-
-//   const result = await res.json();
-
-//   if (!result.ok || !result.data) {
-//     throw new Error(result.message || "Failed to fetch user info");
-//   }
-
-//   return result.data;
-// }
-
 export async function login(nationalCode: number, password: string) {
   return {
     accessToken: "mock_access_token",
